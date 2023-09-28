58fe0f05314626eb91a80b0dabd5ec7802debf64
lab-auto-machine <lab.auto@aeratx.com>
2023-09-27 21:10:16 -0400

    fix: swagger page renders

--- app.py
+++ app.py
@@ -1,26 +1,44 @@
-from flask import Flask, jsonify
+from flask import Flask, send_from_directory, render_template_string
 
 app = Flask(__name__)
 
-# Mock data for the sample endpoint
-mock_data = {
-    "sample123": {
-        "labware": "test-labware",
-        "volume": {
-            "value": 10.5,
-            "unit": "uL"
-        },
-        "conc": "test-conc"
-    }
-}
+@app.route('/api/spec')
+def api_spec():
+    return send_from_directory('.', 'all_webapi_spec.yaml')
 
-@app.route('/sample/<sampleID>', methods=['GET'])
-def get_sample(sampleID):
-    sample_data = mock_data.get(sampleID)
-    if sample_data:
-        return jsonify(sample_data), 200
-    else:
-        return jsonify({"error": "Sample not found"}), 404
+# Swagger UI template
+swagger_ui_template = '''
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Swagger UI</title>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/swagger-ui-dist@3/swagger-ui.css" >
+</head>
+<body>
+    <div id="swagger-ui"></div>
+    <script src="https://cdn.jsdelivr.net/npm/swagger-ui-dist@3/swagger-ui-bundle.js"></script>
+    <script>
+    window.onload = function() {
+        const ui = SwaggerUIBundle({
+            url: "/api/spec",
+            dom_id: '#swagger-ui',
+            presets: [
+                SwaggerUIBundle.presets.apis,
+            ],
+            layout: "BaseLayout",
+            deepLinking: true
+        });
+    }
+    </script>
+</body>
+</html>
+'''
+
+@app.route('/')
+def swagger_ui():
+    return render_template_string(swagger_ui_template)
 
 if __name__ == '__main__':
     app.run(debug=True)
+
